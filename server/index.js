fb654cfa606de376d07df46777977d1760d65b2f
sehoubo <sehoubo@localhost>
2019-01-13 04:10:16 -0500

    nouveau départ pour amstragram

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const cluster = require('cluster')
 const numCPUs = require('os').cpus().length
-const routes = require('./routes/')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
@@ -11,52 +10,49 @@ const morgan = require('morgan')
 const compression = require('compression')
 const cloudinary = require('cloudinary')
 const normalizePort = require('normalize-port')
+const routes = require('./routes/')
 
-const isDev = process.env.NODE_ENV !== 'production';
-// /(([0-2]?[0-9]|3[0-1])\/(0?[13578]|1[02])|([0-2]?[0-9]|30)\/(0?[2469]|11))\/(19[0-9][0-9]|20(0[0-9]|1[0-8]))/gm
+const isDev = process.env.NODE_ENV !== 'production'
+const PORT = process.env.PORT || 5000
+
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`)
+  console.error(`Node cluster master ${process.pid} is running`);
 
   // Fork workers.
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork()
+    cluster.fork();
   }
 
   cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`)
-  })
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-    });
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
 
 } else {
-    const app = express()
-    const PORT = normalizePort(process.env.PORT)
-    const router = express.Router()
-    const URL = process.env.MONGODB_URI
+  const app = express();
+  const PORT = normalizePort(process.env.PORT)
+  const router = express.Router()
+  const URL = process.env.MONGODB_URI
 
-    //middleware chacal
-    app.use(cors())
-    app.use(bodyParser.json())
-    app.use(helmet())
-    app.use(compression())
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  app.use(cors())
+  app.use(bodyParser.json())
+  app.use(helmet())
+  app.use(compression())
 
-    if(isDev)
+  if(isDev)
         app.use(morgan('dev'))
-    else {
-        app.use(express.static(path.resolve(__dirname, '../react-ui/build')))
-        app.use(morgan('common'))
-    }
+  else {
+      app.use(express.static(path.resolve(__dirname, '../react-ui/build')))
+      app.use(morgan('common'))
+  }
 
-    // Priorité les fichiers statics
-
-    routes(router)
+  // Answer API requests.
+  routes(router)
     app.use('/api', router)
 
-      // configuration cloudinary
-    cloudinary.config({
+  cloudinary.config({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_SECRET
@@ -71,12 +67,12 @@ if (!isDev && cluster.isMaster) {
         console.error("can't connect to mongodb")
     }
 
-  // retourne la page de base si on essaie d'accéder à une page qui n'existe pas
-  app.get('*', function(req, res) {
-    res.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'))
-  })
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  });
 
   app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`)
-  })
+    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+  });
 }
